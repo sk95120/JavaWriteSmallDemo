b0c5f67dc3de9d37a6bf4fd42fc953d194edec3d
时轲 <765871277@qq.com>
2018-09-26 16:39:25 +0800

    Update shiwan.java
    更改代码格式。

--- src/shiwan/shiwan.java
+++ src/shiwan/shiwan.java
@@ -13,30 +13,28 @@ public class shiwan {
 	public static void main(String[] args) {
 		int i = 0 ;
 		String path = System.getProperty("user.dir");
-        System.out.println(path);
-        String fileName = path+File.pathSeparator+"File"+
-        File.pathSeparator+"1.txt";
-        File file = new File(fileName);
-        if(file.exists()){
-            file.delete();
-        }
-        FileWriter fr;
+		System.out.println(path);
+		String fileName = path+File.pathSeparator+"File"+
+		File.pathSeparator+"1.txt";
+		File file = new File(fileName);
+		if(file.exists()){
+		    file.delete();
+		}
+		FileWriter fr;
 		try {
 			fr = new FileWriter(file);
 			BufferedWriter bw = new BufferedWriter(fr); 
-	        PrintWriter out = new PrintWriter(bw);
-	        for(;i<100000;i++) {
-	        	out.println(i+1);
-	        }
-	        
-	        out.close();
+		PrintWriter out = new PrintWriter(bw);
+		for(;i<100000;i++) {
+			out.println(i+1);
+		}
+
+		out.close();
 
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}   
-
-		
 	}
 
 }
